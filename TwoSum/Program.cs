d6a5a5b7ad8b04be8c85da5948f2997785436c88
Nicolas Mendez <nicomenfede@gmail.com>
2020-06-14 19:46:00 +0100

    Add code from solution and Edit readme.md

---
+++ TwoSum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TwoSum
+{
+    public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
+    {
+        throw new NotImplementedException("Waiting to be implemented.");
+    }
+
+    public static void Main(string[] args)
+    {
+        Tuple<int, int> indices = FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10);
+        if (indices != null)
+        {
+            Console.WriteLine(indices.Item1 + " " + indices.Item2);
+        }
+    }
+}
