304eddc2ecd27b4801b2cee595b4309c25f69c3a
Nicolas Mendez <nicomenfede@gmail.com>
2020-06-15 16:28:47 +0100

    Fill the HashSet in the constructor

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -6,17 +6,13 @@ class TwoSum
 {
     public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
     {
-        var hs = new HashSet<int>();
-        list.ToList().ForEach(x => hs.Add(x));
+        var hs = new HashSet<int>(list);
 
         for (int i = 0; i < hs.Count; i++)
         {
             var diff = sum - list[i];
             if (hs.Contains(diff) && i != list.IndexOf(diff))
-            {
-                var index = list.IndexOf(diff);
-                return new Tuple<int, int>(i, index);
-            }
+                return new Tuple<int, int>(i, list.IndexOf(diff));
         }
 
         return null;
